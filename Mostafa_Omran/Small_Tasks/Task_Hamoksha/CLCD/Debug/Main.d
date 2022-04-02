8baea1f7342f33faa163868e8aa485bce3812d44
unknown <mostafa01639@icloud.com>
2022-04-02 19:07:09 +0200

    Update

---
+++ Mostafa_Omran/Small_Tasks/Task_Hamoksha/CLCD/Debug/Main.d
@@ -0,0 +1,10 @@
+Main.o Main.o: ../Main.c ../STD_TYPES.h ../BIT_MATH.h ../CLCD_interface.h \
+  ../DIO_interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../CLCD_interface.h:
+
+../DIO_interface.h:
