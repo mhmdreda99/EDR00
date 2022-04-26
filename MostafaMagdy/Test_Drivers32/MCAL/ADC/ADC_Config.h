8d229ebb4944df2e9c7fc50e55bdff181b3b9fa2
MostafaMagdy99 <mostafaamagdii7@gmail.com>
2022-04-26 02:27:17 +0200

    ADD: ADC Driver

---
+++ MostafaMagdy/Test_Drivers32/MCAL/ADC/ADC_Config.h
@@ -0,0 +1,29 @@
+/*
+ * ADC_Config.h
+ *
+ *  Created on: Apr 25, 2022
+ *      Author: Seko
+ */
+
+#ifndef MCAL_ADC_ADC_CONFIG_H_
+#define MCAL_ADC_ADC_CONFIG_H_
+
+//Voltage refernce selection
+#define AREF_Internal_VREF
+//#define AREF_External_VREF
+
+//Resolution Selection
+#define Resolution_10_Bit
+//#define Resolution_8_Bit
+
+//Prescaler selection
+#define ADC_Prescaler_2
+#define ADC_Prescaler_4
+#define ADC_Prescaler_8
+#define ADC_Prescaler_16
+#define ADC_Prescaler_32
+#define ADC_Prescaler_64
+#define ADC_Prescaler_128
+
+
+#endif /* MCAL_ADC_ADC_CONFIG_H_ */
