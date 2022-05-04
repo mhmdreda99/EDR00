21c01a72e234087c1e33eab88057a2c5a7377483
Mostafa <mostafa01639@icloud.com>
2022-05-04 05:25:55 +0200

    ADC_Driver

---
+++ Mostafa_Omran/ATMEGA32_Driver/1-MCAL/4-ADC_Driver/ADC_program.c
@@ -0,0 +1,194 @@
+/*******************************************************************************************/
+/*******************************************************************************************/
+/****************************    Auther  : Mostafa Mahmoud      ****************************/
+/****************************    Layer   : MCAL                 ****************************/
+/****************************    SWC     : ADC                  ****************************/
+/****************************    Version : 1.00                 ****************************/
+/*******************************************************************************************/
+/*******************************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "ADC_interface.h"
+#include "ADC_private.h"
+#include "ADC_config.h"
+#include "ADC_register.h"
+
+static u16* ADC_pu8Reading=NULL;
+static void (*ADC_pvCallBackNotificationFunc)(void)=NULL;
+static u16 adcVal;
+
+u8 ADC_u8BusyState=0;
+
+void ADC_voidInit(void)
+{
+	/*AVCC as reference voltage*/
+#if(VREF==0)
+	CLR_BIT(ADMUX,ADMUX_REFS0);
+	CLR_BIT(ADMUX,ADMUX_REFS1);
+#elif(VREF==1)
+	SET_BIT(ADMUX,ADMUX_REFS0);
+	CLR_BIT(ADMUX,ADMUX_REFS1);
+#elif(VREF==2)
+	SET_BIT(ADMUX,ADMUX_REFS0);
+	SET_BIT(ADMUX,ADMUX_REFS1);
+#endif
+
+	/*Activate Left adjust result*/
+#if(Resolution==0)
+	SET_BIT(ADMUX,ADMUX_ADLAR);
+#endif
+
+	/*Set Prescaler to division */
+#if(PS==0)
+	CLR_BIT(ADCSRA,ADCSRA_ADPS2);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS1);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==1)
+	CLR_BIT(ADCSRA,ADCSRA_ADPS2);
+	SET_BIT(ADCSRA,ADCSRA_ADPS1);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==2)
+	CLR_BIT(ADCSRA,ADCSRA_ADPS2);
+	SET_BIT(ADCSRA,ADCSRA_ADPS1);
+	SET_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==3)
+	SET_BIT(ADCSRA,ADCSRA_ADPS2);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS1);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==4)
+	SET_BIT(ADCSRA,ADCSRA_ADPS2);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS1);
+	SET_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==5)
+	SET_BIT(ADCSRA,ADCSRA_ADPS2);
+	SET_BIT(ADCSRA,ADCSRA_ADPS1);
+	CLR_BIT(ADCSRA,ADCSRA_ADPS0);
+#elif(PS==6)
+	SET_BIT(ADCSRA,ADCSRA_ADPS2);
+	SET_BIT(ADCSRA,ADCSRA_ADPS1);
+	SET_BIT(ADCSRA,ADCSRA_ADPS0);
+#endif
+	/*Enable peripheral */
+	SET_BIT(ADCSRA,ADCSRA_ADEN);
+}
+
+u8 ADC_u8StartConversionSynch(u8 Copy_u8Channel,u16*Copy_pu8Reading)
+{
+	u32 Local_u32Counter=0;
+	u8 Local_u8ErrorState=0;
+	if(ADC_u8BusyState==0)
+	{
+		/*ADC is now busy*/
+		ADC_u8BusyState=1;
+
+		/*Clear the MUX bits in ADMUX register*/
+		ADMUX &= MUXMask;
+
+		/*Set the required channel into the MUX bits*/
+		ADMUX |= Copy_u8Channel;
+
+		/*Start conversion*/
+		SET_BIT(ADCSRA,ADCSRA_ADSC);
+
+		/*Polling (busy waiting) until the conversion complete flag is set or counter reaching timeout value*/
+		while(((GET_BIT(ADCSRA,ADCSRA_ADIF))==0) && (Local_u32Counter!=ADC_u32TIMEOUT) )
+		{
+			Local_u32Counter++;
+		}
+		if(Local_u32Counter==ADC_u32TIMEOUT)
+		{
+			/*Loop is broken because the timeout is reached*/
+			Local_u8ErrorState=1;
+		}
+		else
+		{
+			/*Loop is broken because flag is raised*/
+
+			/*Clear the conversion clear flag*/
+			SET_BIT(ADCSRA,ADCSRA_ADIF);
+#if(Resolution==0)
+			/*Return the reading*/
+			*Copy_pu8Reading=ADCH;
+#elif(Resolution==1)
+			adcVal=0;
+			adcVal = ADCL + (ADCH << 8); //Get the values of the two ADC registers
+			*Copy_pu8Reading = (adcVal & _10_BIT_MASK); //Mask higher bits in ADCH and read only the 10 bits for the ADC
+#endif
+			/*ADC is finished, return it to idle */
+			ADC_u8BusyState=0;
+		}
+
+	}
+	else
+	{
+		Local_u8ErrorState=1;
+	}
+	return Local_u8ErrorState;
+}
+
+u8 ADC_u8StartConversionAsynch(u8 Copy_u8Channel,u16*Copy_pu8Reading,void(*Copy_pvNotificationFunc)(void))
+{
+	u8 Local_u8ErrorState=0;
+	if(ADC_u8BusyState==0)
+	{
+		if(Copy_pu8Reading == NULL || Copy_pvNotificationFunc == NULL)
+		{
+			Local_u8ErrorState=1;
+		}
+		else
+		{
+			/*Make ADC busy in order not to work until being idle 0*/
+			ADC_u8BusyState=1;
+
+			/*Initialize the reading variable globally*/
+			ADC_pu8Reading=Copy_pu8Reading;
+
+			/*Initialize the callback notification function globally*/
+			ADC_pvCallBackNotificationFunc=Copy_pvNotificationFunc;
+
+			/*Clear the MUX bits in ADMUX register*/
+			ADMUX &= MUXMask;
+
+			/*Set the required channel into the MUX bits*/
+			ADMUX |= Copy_u8Channel;
+
+			/*Start conversion*/
+			SET_BIT(ADCSRA,ADCSRA_ADSC);
+
+			/*ADC interrupt enable */
+			SET_BIT(ADCSRA,ADCSRA_ADIE);
+
+		}
+
+	}
+	else
+	{
+		Local_u8ErrorState=1;
+	}
+	return Local_u8ErrorState;
+}
+
+ISR(__vector_16)
+{
+
+#if(Resolution==0)
+	/*Read ADC result*/
+	*ADC_pu8Reading=ADCH;
+#elif(Resolution==1)
+	adcVal=0;
+	adcVal = ADCL + (ADCH << 8); //Get the values of the two ADC registers
+	*ADC_pu8Reading = (adcVal & _10_BIT_MASK); //Mask higher bits in ADCH and read only the 10 bits for the ADC
+#endif
+	/*Make ADC state be idle (0) because it finished */
+	ADC_u8BusyState=0;
+
+	/*Invoke the callback notification function*/
+	(*ADC_pvCallBackNotificationFunc)();
+
+	/*Disable ADC conversion complete interrupt*/
+	CLR_BIT(ADCSRA,ADCSRA_ADIE);
+
+
+}
---
+++ Mostafa_Omran/ATMEGA32_Driver/1-MCAL/4-ADC_Driver/ADC_register.h
@@ -0,0 +1,43 @@
+/*******************************************************************************************/
+/*******************************************************************************************/
+/****************************    Auther  : Mostafa Mahmoud      ****************************/
+/****************************    Layer   : MCAL                 ****************************/
+/****************************    SWC     : ADC                  ****************************/
+/****************************    Version : 1.00                 ****************************/
+/*******************************************************************************************/
+/*******************************************************************************************/
+
+#ifndef ADC_REGISTER_H
+#define ADC_REGISTER_H
+
+
+/********************************ADC INT ISR**********************************/
+#define ISR(vector, ...) \
+	void vector(void) __attribute__ ((signal));\
+	void vector(void)
+
+
+#define ADMUX                       *((volatile u8*)0x27)           //ADC multiplexer selection register
+#define ADMUX_REFS1                 7                               //Referance selection bit1
+#define ADMUX_REFS0                 6                               //Referance selection bit0
+#define ADMUX_ADLAR                 5                               //ADC left adjust result
+
+
+#define ADCSRA                      *((volatile u8*)0x26)           //ADC control and status register A
+#define ADCSRA_ADEN                 7                               //ADC enable
+#define ADCSRA_ADSC                 6                               //Start conversion
+#define ADCSRA_ADATE                5                               //Auto trigger enable
+#define ADCSRA_ADIF                 4                               //Inturrupt flag
+#define ADCSRA_ADIE                 3                               //Inturrupt enable
+#define ADCSRA_ADPS2                2                               //Prescaler bit2
+#define ADCSRA_ADPS1                1                               //Prescaler bit1
+#define ADCSRA_ADPS0                0                               //Prescaler bit0
+
+
+#define ADCH                        *((volatile u8*)0x25)           //ADC high register
+#define ADCL                        *((volatile u8*)0x24)           //ADC low register
+
+#define SFIOR                       *((volatile u8*)0x50)           //Special function I/O register
+
+
+#endif
